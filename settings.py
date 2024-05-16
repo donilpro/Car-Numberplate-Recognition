c936ff2d9219bf5e21495170a058b17c69b66aa3
donilpro <donil858@gmail.com>
2024-05-16 13:22:13 +0300

    Add set and get model methods

--- settings.py
+++ settings.py
@@ -4,9 +4,26 @@ import PyQt5.QtGui
 from PyQt5.uic import loadUi
 from PyQt5.QtCore import pyqtSignal, QByteArray, QBuffer, QIODevice
 from design.ui.settingsUI import Ui_Dialog
+import configparser
 
 
 class SettingsDialog(Ui_Dialog, QDialog):
     def __init__(self):
         super().__init__()
+        config = configparser.ConfigParser()
+        config.read('settings.ini')
+        self._clsModel = config['PATHS']['ClsModel']
+        self._detModel = config['PATHS']['DetModel']
         self.setupUi(self)
+
+    def get_cls_model(self):
+        return self._clsModel
+
+    def get_det_model(self):
+        return self._detModel
+
+    def _set_cls_model(self, model):
+        self._clsModel = model
+
+    def _set_det_model(self, model):
+        self._detModel = model
